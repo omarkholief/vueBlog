240b01b721abda905e4b87e5908189c238bbe2b1
Omar <Omaribrahim@email.com>
2025-04-29 18:01:09 +0300

    sfdsdsd

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="max-w-7xl container item-center mx-auto">
+    <nav class="flex justify-between bg-white shadow py-4">
+      <a href="/" class="text-xl font-semibold">Blogloo</a>
+      <div class="flex gap-2 items-center">
+        <p class="cursor-pointer">Light</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="moon"
+          class="lucide lucide-moon top-icon w-5 h-5 light cursor-pointer">
+          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+        </svg>
+        <p class="cursor-pointer">Dark</p>
+      </div>
+    </nav>
+  </header>
+  <section class="max-w-7xl container item-center text-center my-20 mx-auto">
+    <h1 class="text-4xl font-bold">Blogs Section For Everyone</h1>
+    <p class=" my-5 text-gray-500 dark:text-gray-400 text-lg font-medium">Lorem Ipsum is simply dummy text of the printing
+      and typesetting industry.</p>
+  </section>
+  <section class="max-w-7xl container item-center mx-auto">
+    <ul></ul>
+  </section>
+</template>
+
+<style scoped></style>
